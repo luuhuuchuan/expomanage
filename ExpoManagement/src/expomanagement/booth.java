50aa1cf997d2b139ef08f8e9a90bd80d674b44fc
vuntgc00093 <vuntgc00093@57dd15be-692a-1d84-9c25-135207f6f48a>
2010-11-08 04:57:17 +0000

    Vu tao class Booth

---
+++ ExpoManagement/src/expomanagement/booth.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package expomanagement;
+
+/**
+ *
+ * @author namh3o
+ */
+public class booth {
+
+}
