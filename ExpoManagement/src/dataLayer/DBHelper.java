7e5ff8720ef9636b7481568ad09d8deb998c9efd
lamquangki <lamquangki@57dd15be-692a-1d84-9c25-135207f6f48a>
2010-11-12 10:12:38 +0000

    PHAN LAM MAU -------------- Them phan list Expo va code tip add edit , del... Moi nguoi nho tao flie .sql de them cac ban ghi va store de cac ban khac con them vao CSDL .... VD: file Expo.sql
    ------------
    LUU Y : cac icon cho nút minh de o thu muc icon_func mọi người lấy ra mà dùng :D ...

---
+++ ExpoManagement/src/dataLayer/DBHelper.java
@@ -0,0 +1,67 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package dataLayer;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author duydt
+ */
+public class DBHelper {
+    private Connection conn = null;
+    public Connection getConnection(){
+        return conn;
+    }
+    //open the connection
+    public boolean openConnection(){
+        try{
+            //load the driver (type 1)
+            Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
+            //load the driver (type 4)
+            //Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        }
+        catch(Exception ex){
+            JOptionPane.showMessageDialog(null, "Error when resgistering a bridge","Load the driver",JOptionPane.ERROR_MESSAGE);
+            return false;
+        }
+        try{
+            //open the connection (type 1)
+            conn = DriverManager.getConnection("jdbc:odbc:Expo");
+            //(type 4)
+            //conn = DriverManager.getConnection("jdbc:sqlserver://localhost;DatabaseName=Expo","user=sa","password=123456");
+        }
+        catch(Exception ex){
+            JOptionPane.showMessageDialog(null, "Can not open the connection"+ex,"Open the connection",JOptionPane.ERROR_MESSAGE);
+            return false;
+        }
+        return true;
+    }
+
+    //close the connection
+    @Override
+    public void finalize()throws Throwable{
+        if(conn != null){
+            conn.setAutoCommit(true);
+            conn.close();            
+            conn = null;
+        }
+    }
+    //create an interface to send and receive data
+    public Statement getStatement()throws SQLException{
+        return conn.createStatement();
+    }
+     //create an interface to send and receive data
+    public CallableStatement getCallAble(String  storeName)throws SQLException{
+        return conn.prepareCall(storeName);
+    }
+    
+}
